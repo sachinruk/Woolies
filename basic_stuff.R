bd6b5f7da749f5e09238b2b89497cd0c81840e94
Hemanthi <hemanthi@hemanthi-Satellite-L650.(none)>
2015-03-29 18:30:59 +1100

    tried to check correlations

--- basic_stuff.R
+++ basic_stuff.R
@@ -5,14 +5,13 @@ train_data$date=as.Date(train_data$date,format="%d/%m/%y")
 
 library(ggplot2)
 library(gridExtra)
-idx=train_data$product_store_id=='2e';
+idx=train_data$product_store_id=='3e';
 g.top=ggplot()+
   geom_line(aes(x=train_data$date[idx],y=train_data$sales[idx]))
 g.bottom=ggplot()+
   geom_line(aes(x=train_data$date[idx],y=train_data$promotion[idx]))
 
 grid.arrange(g.top,g.bottom, heights = c(4/5, 1/5)) 
-plot(p)
 #take the FFT
 a=fft(train_data$sales[idx])
 qplot(1:sum(idx),log(abs(a)),geom="line")
@@ -20,3 +19,11 @@ qplot(1:sum(idx),log(abs(a)),geom="line")
 #take last weeks sales to predict todays
 err=diff(train_data$sales[idx])^2
 rmse=sqrt(sum(err)/length(err))
+
+#aggregate products and see correlations
+products=unique(train_data$product_name)
+product_shops=unique(train_data$product_store_id)
+x=matrix()
+for (i in 1:44){
+  x=rbind(x,train_data$sales[train_data$product_store_id==product_shops[i]])
+}
